1b4aeb740fbf3eeb606d6776d4866e3cea196c4e
shariquearshad <shariquearshad@gmail.com>
2020-12-10 20:02:50 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../styles/App.css";
+const description =
+  "an app where you can order from local shop and pickup at set time to avoide queue";
 
 export default function App() {
   return (
-    <div className="App" id="root">
-      <p>I am learning React. My life is getting better. </p>
+    <div className="App">
+      <h1 data-ns-test="project-name">Local Grocery</h1>
+      <h3 data-ns-test="project-description">{description}</h3>
     </div>
   );
 }
